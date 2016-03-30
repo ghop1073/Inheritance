994a11c821e85c43d039d71c538df389988c65f8
ghop1073 <gagehopkins1@gmail.com>
2016-03-30 12:16:15 -0600

    added bin/ to ignored files and fixed putting class

--- src/model/Putting.java
+++ src/model/Putting.java
@@ -8,27 +8,32 @@ public class Putting
 	private ArrayList<String> putts;
 	private int greenLevel;
 	private int greenSpeed;
-	
-	public ArrayList<String> getPutts;
+
+	public ArrayList<String> getPutts()
 	{
 		return putts;
 	}
+
 	public void setPutts(ArrayList<String> putts)
 	{
 		this.putts = putts;
 	}
+
 	public int getGreenLevel()
 	{
 		return greenLevel;
 	}
+
 	public void setGreenLevel(int greenLevel)
 	{
 		this.greenLevel = greenLevel;
 	}
+
 	public int getGreenSpeed()
 	{
 		return greenSpeed;
 	}
+
 	public void setGreenSpeed(int greenSpeed)
 	{
 		this.greenSpeed = greenSpeed;
