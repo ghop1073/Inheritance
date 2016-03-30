f851fdd7ae016f830af8d74ddf5ce44d926e2dac
ghop1073 <gagehopkins1@gmail.com>
2016-03-30 12:13:07 -0600

    added in games class and added to putting class

--- src/model/Putting.java
+++ src/model/Putting.java
@@ -1,6 +1,36 @@
 package model;
 
+import java.util.ArrayList;
+
 public class Putting
 {
 
+	private ArrayList<String> putts;
+	private int greenLevel;
+	private int greenSpeed;
+	
+	public ArrayList<String> getPutts;
+	{
+		return putts;
+	}
+	public void setPutts(ArrayList<String> putts)
+	{
+		this.putts = putts;
+	}
+	public int getGreenLevel()
+	{
+		return greenLevel;
+	}
+	public void setGreenLevel(int greenLevel)
+	{
+		this.greenLevel = greenLevel;
+	}
+	public int getGreenSpeed()
+	{
+		return greenSpeed;
+	}
+	public void setGreenSpeed(int greenSpeed)
+	{
+		this.greenSpeed = greenSpeed;
+	}
 }
